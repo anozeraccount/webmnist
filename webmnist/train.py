e8a985dc1e824310d0ae9491fc558d60fb9cf917
DohnnyJepp <thaliuk78@gmail.com>
2021-03-30 11:09:33 +0200

    add

---
+++ webmnist/train.py
@@ -0,0 +1,34 @@
+import torch
+import torchvision
+import tqdm
+
+class LeNet5(nn.Module):
+    def __init__(self, n_classes: int) -> None:
+        super(LeNet5, self).__init__()
+        self.n_classes = n_classes
+
+        self.convolutions = nn.Sequential(
+            nn.Conv2d(1, 8, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2),
+
+            nn.Conv2d(8, 16, kernel_size=5),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(16 * 5 * 5, 128),
+            nn.ReLU(inplace=True),
+
+            nn.Linear(128, 64),
+            nn.ReLU(inplace=True),
+
+            nn.Linear(64, self.n_classes),
+            nn.LogSoftmax(dim=-1),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.convolutions(x)
+        x = x.reshape(x.size(0), -1)
+        x = self.classifier(x)
+        return x
